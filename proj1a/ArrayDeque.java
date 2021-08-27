a2ab231ea9be0857903cdd6862f58c156dd15d24
Chatzh <xiaoxinerw@gmail.com>
2021-08-28 01:58:39 +0900

    Disable resize down

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -27,13 +27,6 @@ public class ArrayDeque<T> {
             System.arraycopy(sentinel.array, nextLast, n, size + nextLast, size - nextLast);
             sentinel.array = n;
             nextFirst = size + nextLast - 1;
-        } else if (size + i > 8 && size + i < length / 4) {
-            T[] n = (T[]) new Object[length / 2];
-            System.arraycopy(sentinel.array, nextFirst + 1, n, 1, size - nextLast);
-            System.arraycopy(sentinel.array, 0, n, 2, nextLast);
-            sentinel.array = n;
-            nextFirst = 0;
-            nextLast = size + 1;
         }
     }
 
