8a2b41a05bbac6ca3d81a6b6205be1992fbad74e
Chatzh <xiaoxinerw@gmail.com>
2021-09-06 19:54:17 +0900

    First submission of hw1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,20 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        assertEquals(10, arb.capacity());
+
+        arb.enqueue(0);
+        arb.enqueue(2.3d);
+        arb.enqueue("null");
+
+        assertEquals(3, arb.fillCount());
+        assertEquals(0, arb.dequeue());
+        assertEquals(2.3d, arb.dequeue());
+        assertEquals("null", arb.peek());
+        assertEquals("null", arb.dequeue());
+        assertTrue(arb.isEmpty());
+        //arb.dequeue();
     }
 
     /** Calls tests for ArrayRingBuffer. */
