cdcb8628df386691cc6e142cd8e75c872b6d4446
Chatzh <xiaoxinerw@gmail.com>
2021-09-17 04:33:14 +0900

    Update Percolation.java

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -47,12 +47,11 @@ public class Percolation {
             }
 
             // check if is percolates
-            for (int i = 0; i < grid.length; i++) {
-                if (percolates()) {
-                    break;
-                }
-                if (isFull(grid.length - 1, i)) {
-                    wqu.union(xyTo1D(grid.length - 1, i), bottom);
+            if (isFull(row, col) && !percolates()) {
+                for (int i = 0; i < grid.length; i++) {
+                    if (isFull(grid.length - 1, i)) {
+                        wqu.union(xyTo1D(grid.length - 1, i), bottom);
+                    }
                 }
             }
         }
