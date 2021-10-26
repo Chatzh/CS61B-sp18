b74f29d83f1465ac98f6fae4cf6e840947148ab9
Chatzh <xiaoxinerw@gmail.com>
2021-10-26 23:41:25 +0900

    Finished lab9

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -7,14 +7,14 @@ import java.util.Set;
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
  *
- *  @author Your name here
+ *  @author Chatzh
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
     private static final int DEFAULT_SIZE = 16;
     private static final double MAX_LF = 0.75;
 
-    private ArrayMap<K, V>[] buckets;
+    private ULLMap<K, V>[] buckets;
     private int size;
 
     private int loadFactor() {
@@ -22,7 +22,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     }
 
     public MyHashMap() {
-        buckets = new ArrayMap[DEFAULT_SIZE];
+        buckets = new ULLMap[DEFAULT_SIZE];
         this.clear();
     }
 
@@ -31,7 +31,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     public void clear() {
         this.size = 0;
         for (int i = 0; i < this.buckets.length; i += 1) {
-            this.buckets[i] = new ArrayMap<>();
+            this.buckets[i] = new ULLMap<>();
         }
     }
 
@@ -53,19 +53,52 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("argument to get() is null");
+        }
+        int i = hash(key);
+        return buckets[i].get(key);
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("first argument to put() is null");
+        }
+
+        if (loadFactor() > MAX_LF) {
+            resize(2 * buckets.length);
+        }
+
+        int i = hash(key);
+        if (!buckets[i].containsKey(key)) {
+            size++;
+        }
+
+        buckets[i].put(key, value);
+    }
+
+    private void resize(int size) {
+        MyHashMap temp = new MyHashMap();
+        temp.buckets = new ULLMap[size];
+        temp.clear();
+        
+        for (int i = 0; i < buckets.length; i++) {
+            Iterator<K> itr = buckets[i].iterator();
+            while (itr.hasNext()) {
+                K key = itr.next();
+                temp.put(key, buckets[i].get(key));
+            }
+        }
+
+        buckets = temp.buckets;
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
